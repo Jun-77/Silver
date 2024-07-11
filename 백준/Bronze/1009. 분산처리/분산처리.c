2198d7dae9461c77b0027b94cc705140732093b3
황보은준 <80016088+Jun-77@users.noreply.github.com>
2024-07-11 11:31:45 +0900

    [Bronze II] Title: 분산처리, Time: 736 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int count;
+	int a, b;
+	scanf("%d", &count);
+
+	for (int i = 0; i < count; i++) {
+		scanf("%d %d", &a, &b);
+		getchar();
+
+		int res = 1;
+		for (int j = 0; j < b; j++) {
+			res = (res * a) % 10;
+		}
+		if (res == 0) printf("10\n");
+		else printf("%d\n", res);
+	}
+	return 0;
+}
