9e575838ec978090aafd55cb6e40f0fb473689b9
황보은준 <80016088+Jun-77@users.noreply.github.com>
2024-08-18 20:01:59 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int N, M;
+int arr[8];
+
+void DFS(int depth, int num) {
+    int i;
+    if (depth == M) {
+        for (i = 0; i < M; i++)
+            printf("%d ", arr[i]);
+        printf("\n");
+    }
+    else {
+        for (i = 1; i <= N; i++) {
+            if (num <= i){
+                arr[depth] = i;
+                DFS(depth + 1, i);
+            }
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &N, &M);
+    DFS(0, 0);
+    return 0;
+}
