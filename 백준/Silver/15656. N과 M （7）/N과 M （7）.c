48d6c89195b0bd52cc3c4846eed243e374b48728
황보은준 <80016088+Jun-77@users.noreply.github.com>
2024-08-28 22:38:27 +0900

    [Silver III] Title: N과 M (7), Time: 520 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int N, M;
+int res[8];
+int num[8];
+
+void DFS(int depth) {
+	if (depth == M) {
+		for (int i = 0; i < M; i++) {
+			printf("%d ", res[i]);
+		}
+		printf("\n");
+		return;
+	}
+	for (int i = 0; i < N; i++) {
+		res[depth] = num[i];
+		DFS(depth + 1);
+	}
+}
+
+int main() {
+	scanf("%d %d", &N, &M);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &num[i]);
+	}
+
+	// 사전순 출력을 위해 배열 정렬
+	for (int i = 0; i < N - 1; i++) {
+		for (int j = 0; j < N - 1 - i; j++) {
+			if (num[j] > num[j + 1]) {
+				int temp = num[j];
+				num[j] = num[j + 1];
+				num[j + 1] = temp;
+			}
+		}
+	}
+
+	DFS(0);
+	return 0;
+}
