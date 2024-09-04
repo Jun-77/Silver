c357b8babe4ce6283c7a8018e88181aac0c0f4bf
황보은준 <80016088+Jun-77@users.noreply.github.com>
2024-09-04 15:39:34 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.c
+++ 백준/Silver/11047. 동전 0/동전 0.c
@@ -2,35 +2,19 @@
 #include <stdio.h>
 
 int main() {
-	int N, K, count = 0, coin[10];
-	scanf("%d %d", &N, &K);
-	for (int i = 0; i < N; i++) {
-		scanf("%d", &coin[i]);
-	}
+    int N, K, count = 0, coin[10];
+    scanf("%d %d", &N, &K);
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &coin[i]);
+    }
 
-	if (N == 1) {
-		printf("%d", K);
-		return 0;
-	}
+    for (int i = N - 1; i >= 0; i--) {  // 큰 단위 동전부터 탐색
+        if (coin[i] <= K) {              // 현재 동전으로 줄일 수 있으면
+            count += K / coin[i];        // 사용할 동전 개수를 더함
+            K %= coin[i];                // 남은 금액 갱신
+        }
+    }
 
-	while (K != 0) {
-		for (int i = 0; i < N; i++) {
-			if (coin[i] == K) {
-				K = K - coin[i];
-				count++;
-				break;
-			}
-			else if (coin[i] > K) {
-				K = K - coin[i - 1];
-				count++;
-				break;
-			}
-			else if (i == N - 1) {
-				K = K - coin[i];
-				count++;
-			}
-		}
-	}
-	printf("%d", count);
-	return 0;
+    printf("%d", count);              
+    return 0;
 }
