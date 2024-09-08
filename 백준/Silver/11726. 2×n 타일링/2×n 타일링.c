cda3fea8f070a0aa71284bfda7135e125abef40a
황보은준 <80016088+Jun-77@users.noreply.github.com>
2024-09-08 16:29:19 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int n;
+	int dp[1001];
+	scanf("%d", &n);
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+	}
+	int res = dp[n];
+	printf("%d", res);
+}
